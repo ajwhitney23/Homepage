bb387249cbd5fe3471220f96e767396d3866d577
ajwhitney23 <ajwhitney@wpi.edu>
2020-10-12 15:31:54 -0400

    can read json now

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
-var json = JSON.parse("bookmarks.json");
+function readJSON(file) {
+    fetch(file)
+        .then(response => {return response.json()})
+        .then(jsonData => {
+            createLinks(jsonData);
+        })
+}
 
-function createLinks() {
-    var links = json.links;
+function createLinks(data) {
+    console.log(data);
+    var links = data.links;
     div = document.getElementById("container");
-    links.forEach(element => {
-        linkName = element["name"];
-        linkURL = element["url"];
+    for(i = 0; i < links.length; i++) {
+        linkName = "\t" + links[i].name + "\n";
+        linkURL = links[i].url;
 
         a = document.createElement("a");
         ref = document.createAttribute("href");
@@ -15,6 +22,39 @@ function createLinks() {
         a.textContent = linkName;
 
         div.appendChild(a);
-    });
+    }
+    addText(closingText);
+    // js way???
+    // div = document.getElementById("container");
+    // links.forEach(element => {
+    //     linkName = element["name"];
+    //     linkURL = element["url"];
 
+    //     a = document.createElement("a");
+    //     ref = document.createAttribute("href");
+    //     ref.value = linkURL;
+    //     a.setAttributeNode(ref);
+    //     a.setAttribute('target', '_blank');
+    //     a.textContent = linkName;
+
+    //     div.appendChild(a);
+    // });
+
+}
+
+var openingText = "Bookmarks : {"
+var closingText = "}"
+
+function addText(text) {
+    div = document.getElementById("container");
+    p = document.createElement("p");
+    p.textContent = text;
+
+    div.appendChild(p);
+}
+
+
+function initBookmarks() {
+    addText(openingText);
+    readJSON("bookmarks.json");
 }
