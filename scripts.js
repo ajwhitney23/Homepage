3d56e4101cc6696e04e86234679527f289e0707c
ajwhitney23 <ajwhitney@wpi.edu>
2020-10-13 09:00:09 -0400

    script cleanup

--- scripts.js
+++ scripts.js
@@ -34,27 +34,8 @@ function createLinks(data) {
 
         div.appendChild(a);
     }
-   // addText(closingText);
-    // js way???
-    // div = document.getElementById("container");
-    // links.forEach(element => {
-    //     linkName = element["name"];
-    //     linkURL = element["url"];
-
-    //     a = document.createElement("a");
-    //     ref = document.createAttribute("href");
-    //     ref.value = linkURL;
-    //     a.setAttributeNode(ref);
-    //     a.setAttribute('target', '_blank');
-    //     a.textContent = linkName;
-
-    //     div.appendChild(a);
-    // });
 
 }
-
-var openingText = "Bookmarks : {"
-var closingText = "}"
 
 function addText(text) {
     div = document.getElementById("container");
@@ -66,6 +47,5 @@ function addText(text) {
 
 
 function initBookmarks() {
-    //addText(openingText);
     readJSON("bookmarks.json");
 }
